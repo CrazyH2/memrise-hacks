0ed5ecfa5344ebf545856d2806c91762162d2ab1
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-06-06 14:43:10 +0100

    Create Review-Mode.js

---
+++ Review-Mode.js
@@ -0,0 +1,8 @@
+ // © Copyright 2024 CrazyH
+ // This file was originally made by CrazyH
+ // Do not rebrand if you are distributing it
+ // © Copyright 2024 CrazyH
+
+setInterval(function(){
+  if (document.getElementsByClassName("sc-1v1crxt-9 kFphHP")[0]) document.getElementsByClassName("sc-1v1crxt-9 kFphHP")[0].click()
+}, 10);
